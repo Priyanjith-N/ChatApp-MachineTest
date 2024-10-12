dc092509bb496cadb061e2f7179ebd0bad7579cc
Priyanjith N <jithpriyan2006@gmail.com>
2024-10-12 14:37:04 +0530

    right hand side when no chat openend page done

--- Client/src/app/features/home-page/home-page.component.ts
+++ Client/src/app/features/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    RouterOutlet
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
